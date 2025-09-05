0d9b1690fde24c2428ae7adffcc18b9eccc616bb
NekoZ410 <1chumeocon@gmail.com>
2025-09-05 09:22:36 +0700

    change and improve shortsToVideo convert performance

--- content-utilities.js
+++ content-utilities.js
@@ -1,4 +1,4 @@
-// utilities: shortsToVideo
+// shortsToVideo: convert URL shorts to video
 function convertShortsToVideo(isEnabled) {
     if (isEnabled) {
         const currentURL = window.location.href;
@@ -14,30 +14,4 @@ function convertShortsToVideo(isEnabled) {
 // init and caching settings on load
 chrome.storage.local.get("utilities-shortsToVideo", (result) => {
     convertShortsToVideo(result["utilities-shortsToVideo"]);
-});
-
-// utilities: navigation shorts to video
-window.addEventListener("popstate", () => {
-    chrome.storage.local.get("utilities-shortsToVideo", (result) => {
-        convertShortsToVideo(result["utilities-shortsToVideo"]);
-    });
-});
-const observer = new MutationObserver(() => {
-    // check if the URL has changed
-    const newURL = window.location.href;
-    if (newURL !== observer.lastUrl) {
-        observer.lastUrl = newURL;
-        chrome.storage.local.get("utilities-shortsToVideo", (result) => {
-            convertShortsToVideo(result["utilities-shortsToVideo"]);
-        });
-    }
-});
-observer.lastUrl = window.location.href;
-observer.observe(document.body, { childList: true, subtree: true });
-
-// listening for changes
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (namespace === "local" && changes["utilities-shortsToVideo"] !== undefined) {
-        convertShortsToVideo(changes["utilities-shortsToVideo"].newValue);
-    }
 });
